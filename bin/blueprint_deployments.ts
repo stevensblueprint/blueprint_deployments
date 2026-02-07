b8b30edc064a75dc611f990361e40edb4e5aa34d
miguel-merlin <mmerlin@stevens.edu>
2026-02-07 17:00:30 -0500

    chore: updated stack name to blueprint-deployments-pipeline-stack

--- bin/blueprint_deployments.ts
+++ bin/blueprint_deployments.ts
@@ -4,7 +4,7 @@ import { config } from "./config";
 import { PipelineStack } from "../lib/stacks/pipeline-stack";
 
 const app = new cdk.App();
-new PipelineStack(app, "PipelineStack", {
+new PipelineStack(app, "blueprint-deployments-pipeline-stack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
